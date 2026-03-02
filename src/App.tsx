8718cc6191db1ce2057dfcae539b565602147a87
RmzGerardo <lalitomendez132@gmail.com>
2026-03-02 17:39:33 -0600

    Integracion del nuevo sugo

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// Importa los estilos de Bootstrap y PrimeReact
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+
+export const App = () => {
+  return (
+    <>
+      <div>Nuevo sugo</div>
+    </>
+  );
+};
