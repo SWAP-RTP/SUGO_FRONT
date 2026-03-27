9bf519600a1685fa45546f6752172610a87202e9
RmzGerardo <lalitomendez132@gmail.com>
2026-03-27 15:01:55 -0600

    v4

--- src/Despacho/components/FormularioDespacho.tsx
+++ src/Despacho/components/FormularioDespacho.tsx
@@ -5,9 +5,10 @@ import { Button } from "primereact/button";
 import { useState } from "react";
 // hooks personalizados
 import { useHook_General } from "../../General/hooks/useHook";
-import { Servicio } from "./Servicio";
+// import { Servicio } from "./Servicio";
 import { Card_Eco } from "../../General/components/Card_Eco";
 import { Pv_estados } from "../../General/components/Pv_estados";
+import "../css/despacho.css";
 
 export const FormularioDespacho = () => {
   const { modulosOptions, motivosOptions } = useHook_General();
@@ -29,77 +30,98 @@ export const FormularioDespacho = () => {
     <>
       <TabView className="tabView">
         <TabPanel className="tabpanel" header="Despacho">
-          <div className="despacho-contenedor d-flex flex-row justify-content-center align-items-start gap-5">
-            {/* <Card_Eco /> */}
-            <div className="card">
-              <div className="titulo">
-                <h1>Formulario de despacho</h1>
-                <hr className="linea_despacho" />
-              </div>
-
-              <div className="mt-4 py-2 p-4 d-flex justify-content-center col-12 md:col-6">
-                <div className="w-100">
-                  {/* Fila para Módulo y Económico */}
-                  <div className="d-flex flex-row gap-2 mb-3 justify-content-center">
-                    <span className="p-float-label flex-grow-1">
-                      <Dropdown
-                        inputId="dd-modulo"
-                        value={selectModulo}
-                        onChange={(e) => setSelectModulo(e.value)}
-                        options={modulosOptions}
-                        className="select"
-                      />
-                      <label htmlFor="dd-modulo">Modulo</label>
-                    </span>
-                    <span className="p-float-label flex-grow-1">
-                      <InputText className="select" />
-                      <label htmlFor="economico">Economico</label>
-                    </span>
+          <div className="container mt-4">
+            <div className="row justify-content-center">
+              <div className="col-12 col-md-6 col-lg-4">
+                {/* <Card_Eco /> */}
+                <div className="card p-2">
+                  <div className="titulo">
+                    <h1>Formulario de despacho</h1>
+                    <hr className="linea_despacho" />
                   </div>
-                  {/* Fila para Motivos */}
-                  <div className="mb-3 d-flex justify-content-center">
-                    <span className="p-float-label w-100">
-                      <Dropdown
-                        className="select motivos"
-                        inputId="dd-motivos"
-                        value={motivos_select}
-                        onChange={(e) => setMotivos_select(e.value)}
-                        options={motivosOptions}
-                        optionLabel="desc"
-                        optionValue="value"
-                      />
-                      <label htmlFor="dd-motivos">Motivos</label>
-                    </span>
+
+                  <div className="row">
+                    <div className="col-6 d-flex justify-content-center">
+                      <div>
+                        <span className="p-float-label input-modulo">
+                          <Dropdown
+                            inputId="dd-modulo"
+                            value={selectModulo}
+                            onChange={(e) => setSelectModulo(e.value)}
+                            options={modulosOptions}
+                            className="select"
+                          />
+                          <label htmlFor="dd-modulo">Modulo</label>
+                        </span>
+                      </div>
+
+                      <div>
+                        <span className="p-float-label input-economico">
+                          <InputText className="select" />
+                          <label htmlFor="economico">Economico</label>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="mt-4 py-2 p-4 d-flex justify-content-center col-12">
+                    <div className="w-100 formulario-inputs">
+                      {/* Fila para Módulo, Económico y Motivos - responsive */}
+                      <div className="d-flex flex-wrap gap-2 mb-3 justify-content-center">
+                        <span className="p-float-label input-motivos">
+                          <Dropdown
+                            className="select"
+                            inputId="dd-motivos"
+                            value={motivos_select}
+                            onChange={(e) => setMotivos_select(e.value)}
+                            options={motivosOptions}
+                            optionLabel="desc"
+                            optionValue="value"
+                          />
+                          <label htmlFor="dd-motivos">Motivos</label>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="d-flex justify-content-center gap-3 mt-4 mb-4">
+                    <Button
+                      icon="pi pi-check"
+                      label="Enviar"
+                      severity="success"
+                    />
+                    <Button
+                      icon="pi pi-times"
+                      label="Limpiar"
+                      severity="danger"
+                    />
                   </div>
                 </div>
-              </div>
 
-              {/* componente dinamico de servicio */}
-              {motivos_select?.desc === "SERVICIO" && <Servicio />}
+                {/* componente dinamico de servicio */}
+                {/* {motivos_select?.desc === "SERVICIO" && <Servicio />} */}
 
-              {/* fecha y hora debajo de los inputs principales */}
-              <div className="d-flex flex-row gap-3 mt-2 py-2 px-2 justify-content-center">
-                <div className="flex align-items-center">
-                  <InputText
-                    value={horaActual}
-                    readOnly
-                    placeholder="Hora"
-                    disabled
-                  />
-                </div>
-                <div className="flex align-items-center">
-                  <InputText
-                    value={fechaActual}
-                    readOnly
-                    placeholder="Fecha"
-                    disabled
-                  />
-                </div>
-              </div>
-
-              <div className="d-flex justify-content-center gap-3 mt-4 mb-4">
-                <Button icon="pi pi-check" label="Enviar" severity="success" />
-                <Button icon="pi pi-times" label="Limpiar" severity="danger" />
+                {/* fecha y hora debajo de los inputs principales */}
+                {/* <div className="d-flex flex-row gap-3 mt-2 py-2 px-2 justify-content-center">
+                    <div className="flex align-items-center">
+                      <InputText
+                        value={horaActual}
+                        readOnly
+                        placeholder="Hora"
+                        disabled
+                        className="select-fh"
+                      />
+                    </div>
+                    <div className="flex align-items-center">
+                      <InputText
+                        value={fechaActual}
+                        readOnly
+                        placeholder="Fecha"
+                        disabled
+                        className="select-fh"
+                      />
+                    </div>
+                  </div> */}
               </div>
             </div>
           </div>
--- src/Despacho/components/Servicio.tsx
+++ src/Despacho/components/Servicio.tsx
@@ -1,90 +1,87 @@
-import { InputText } from "primereact/inputtext";
-import { Dropdown } from "primereact/dropdown";
-import { useState } from "react";
-// hooks personalizados
-import { useHook_General } from "../../General/hooks/useHook";
+// import { InputText } from "primereact/inputtext";
+// import { Dropdown } from "primereact/dropdown";
+// import { useState } from "react";
+// // hooks personalizados
+// import { useHook_General } from "../../General/hooks/useHook";
 
-export const Servicio = () => {
-  const { modalidadesOptions, rutasOptions } = useHook_General();
+// export const Servicio = () => {
+//   const { modalidadesOptions, rutasOptions } = useHook_General();
 
-  // estado para almacenar la modalidad seleccionada
-  const [modalidadSelect, setModalidadSelect] = useState(null);
-  // estado para las rutas seleccionada
-  const [rutaSelect, setRutaSelect] = useState(null);
+//   // estado para almacenar la modalidad seleccionada
+//   const [modalidadSelect, setModalidadSelect] = useState(null);
+//   // estado para las rutas seleccionada
+//   const [rutaSelect, setRutaSelect] = useState(null);
 
-  const ecoDe = [
-    { label: "Planta", value: "Planta" },
-    { label: "Postura", value: "Postura" },
-  ];
+//   const ecoDe = [
+//     { label: "Planta", value: "Planta" },
+//     { label: "Postura", value: "Postura" },
+//   ];
 
-  return (
-    <>
-      {/* primera fila */}
-      <div className="d-flex flex-row gap-3 mt-4 py-2 px-2 justify-content-center">
-        {/* credencial */}
-        <span className="p-float-label">
-          <InputText className="select" />
-          <label htmlFor="username">Credencial</label>
-        </span>
+//   return (
+//     <>
+//       {/* primera fila - Credencial y Turno */}
+//       <div className="d-flex flex-wrap gap-2 py-2 px-2 justify-content-center">
+//         <span className="p-float-label input-servicio">
+//           <InputText className="select" />
+//           <label htmlFor="credencial">Credencial</label>
+//         </span>
 
-        {/* turno */}
-        <span className="p-float-label">
-          <InputText className="select" />
-          <label htmlFor="username">Turno</label>
-        </span>
+//         <span className="p-float-label input-servicio">
+//           <InputText className="select" />
+//           <label htmlFor="turno">Turno</label>
+//         </span>
+//       </div>
 
-        {/* eco de  */}
-        <span className="p-float-label">
-          <Dropdown inputId="dd-city" className="select" options={ecoDe} />
-          <label htmlFor="dd-city">Eco de</label>
-        </span>
-      </div>
+//       {/* segunda fila - Eco de y No.Extintor */}
+//       <div className="d-flex flex-wrap gap-2 mt-3 py-2 px-2 justify-content-center">
+//         <span className="p-float-label input-servicio">
+//           <Dropdown inputId="dd-ecoDe" className="select" options={ecoDe} />
+//           <label htmlFor="dd-ecoDe">Eco de</label>
+//         </span>
 
-      {/* segunda fila */}
-      <div className="d-flex flex-row gap-3 mt-4 py-2 px-2 justify-content-center">
-        {/* credencial */}
-        <span className="p-float-label">
-          <InputText className="select" />
-          <label htmlFor="username">No.Extintor</label>
-        </span>
-        {/* Modalidad  */}
-        <span className="p-float-label">
-          <Dropdown
-            inputId="dd-city"
-            className="select"
-            options={modalidadesOptions}
-            value={modalidadSelect}
-            onChange={(e) => setModalidadSelect(e.value)}
-          />
-          <label htmlFor="dd-city">Modalidad</label>
-        </span>
-        {/* Ruta  */}
-        <span className="p-float-label">
-          <Dropdown
-            inputId="dd-city"
-            className="select"
-            options={rutasOptions}
-            value={rutaSelect}
-            onChange={(e) => setRutaSelect(e.value)}
-          />
-          <label htmlFor="dd-city">Ruta</label>
-        </span>
-      </div>
+//         <span className="p-float-label input-servicio">
+//           <InputText className="select" />
+//           <label htmlFor="extintor">No.Extintor</label>
+//         </span>
+//       </div>
 
-      {/* TERCERA fila */}
-      <div className="d-flex flex-row gap-3 mt-4 py-2 px-2 justify-content-center">
-        {/* credencial */}
-        <span className="p-float-label">
-          <InputText className="select" />
-          <label htmlFor="username">CC</label>
-        </span>
+//       {/* tercera fila - Modalidad y Ruta */}
+//       <div className="d-flex flex-wrap gap-2 mt-3 py-2 px-2 justify-content-center">
+//         <span className="p-float-label input-servicio">
+//           <Dropdown
+//             inputId="dd-modalidad"
+//             className="select"
+//             options={modalidadesOptions}
+//             value={modalidadSelect}
+//             onChange={(e) => setModalidadSelect(e.value)}
+//           />
+//           <label htmlFor="dd-modalidad">Modalidad</label>
+//         </span>
 
-        {/* Modalidad  */}
-        <span className="p-float-label">
-          <InputText className="select" />
-          <label htmlFor="username">Entrada de Operador</label>
-        </span>
-      </div>
-    </>
-  );
-};
+//         <span className="p-float-label input-servicio">
+//           <Dropdown
+//             inputId="dd-ruta"
+//             className="select"
+//             options={rutasOptions}
+//             value={rutaSelect}
+//             onChange={(e) => setRutaSelect(e.value)}
+//           />
+//           <label htmlFor="dd-ruta">Ruta</label>
+//         </span>
+//       </div>
+
+//       {/* cuarta fila - CC y Entrada Operador */}
+//       <div className="d-flex flex-wrap gap-2 mt-3 py-2 px-2 justify-content-center">
+//         <span className="p-float-label input-servicio">
+//           <InputText className="select" />
+//           <label htmlFor="cc">CC</label>
+//         </span>
+
+//         <span className="p-float-label input-servicio">
+//           <InputText className="select" />
+//           <label htmlFor="entrada">Entrada Operador</label>
+//         </span>
+//       </div>
+//     </>
+//   );
+// };
