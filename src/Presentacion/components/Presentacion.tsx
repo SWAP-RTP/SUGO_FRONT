2c5b7c7ab56c4cae54235ac62914f2f6f23617e4
RmzGerardo <lalitomendez132@gmail.com>
2026-05-11 17:45:42 -0600

    catalogo de eco teminado2

--- src/Presentacion/components/Presentacion.tsx
+++ src/Presentacion/components/Presentacion.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import { TabView, TabPanel } from "primereact/tabview";
-import { QRCodeSVG } from "qrcode.react";
 import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
 import { Dropdown } from "primereact/dropdown";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Messages } from "primereact/messages";
 import { useHook_General } from "../../General/hooks/useHook";
 
 export const Hora_Presentacion = () => {
-  const { modulosOptions } = useHook_General();
+  const { modulosOptions, ecoDisponibles } = useHook_General();
 
   // Estado local para el módulo seleccionado
   const [selectedModulo, setSelectedModulo] = useState(null);
@@ -16,87 +18,123 @@ export const Hora_Presentacion = () => {
     <>
       <TabView>
         <TabPanel className="tabpanel" header="Hora de Presentacion">
-          <div className="d-flex justify-content-center">
-            {/* card */}
-            <div className="card_presentacion">
-              {/* titulo */}
-              <div className="titulo">
-                <h1>Hora de Presentación</h1>
-                <hr />
+          <div className="container">
+            <div className="d-flex justify-content-between">
+              <div className="row">
+                {/* card */}
+                <div className="card_presentacion">
+                  {/* titulo */}
+                  <div className="titulo">
+                    <h1>Hora de Presentación</h1>
+                    <hr />
+                  </div>
+
+                  <div className="d-flex align-items-center gap-4 justify-content-center">
+                    {/* credencial */}
+                    <span className="p-float-label w-100">
+                      <InputText className="select" />
+                      <label htmlFor="Credencial">Credencial</label>
+                    </span>
+
+                    {/* Modulo */}
+                    <span className="p-float-label w-100">
+                      <Dropdown
+                        inputId="Modulo"
+                        className="select"
+                        options={modulosOptions}
+                        value={selectedModulo}
+                        onChange={(e) => setSelectedModulo(e.value)}
+                        placeholder="Módulo"
+                      />
+                      <label htmlFor="Modulo">Modulo</label>
+                    </span>
+                  </div>
+
+                  <div
+                    className="d-flex align-items-center gap-4 justify-content-center"
+                    style={{ paddingTop: "1.5rem" }}
+                  >
+                    {/* Hora */}
+                    <span className="p-float-label input-presentacion">
+                      <InputText className="select" />
+                      <label htmlFor="Hora">Hora</label>
+                    </span>
+                    {/* Fecha */}
+                    <span className="p-float-label input-presentacion">
+                      <InputText className="select" />
+                      <label htmlFor="Fecha">Fecha</label>
+                    </span>
+                  </div>
+
+                  <div className="d-flex justify-content-center gap-3 mt-5 ">
+                    <Button
+                      icon="pi pi-save"
+                      className="p-button-sm small"
+                      label="Guardar"
+                      severity="success"
+                      style={{ height: "50px" }}
+                    />
+                    <Button
+                      icon="pi pi-times"
+                      className="p-button-sm small"
+                      label="Limpiar"
+                      severity="danger"
+                      style={{ height: "50px" }}
+                    />
+                  </div>
+                </div>
               </div>
 
-              <div className="d-flex align-items-center gap-4 mt-4 justify-content-center" style={{ paddingTop: "1.5rem" }}>
-                 {/* economico */}
-                <span className="p-float-label w-100">
-                  <InputText
-                    className="select"
-                  />
-                  <label htmlFor="economico">Economico</label>
-                </span>
-                 {/* credencial */}
-                <span className="p-float-label w-100">
-                  <InputText
-                    className="select"
-                  />
-                  <label htmlFor="Credencial">Credencial</label>
-                </span>
-
-                
-                 {/* Modulo */}
-                <span className="p-float-label w-100">
-                  <Dropdown
-                    inputId="Modulo"
-                    className="select"
-                    options={modulosOptions}
-                    value={selectedModulo}
-                    onChange={(e) => setSelectedModulo(e.value)}
-                    placeholder="Módulo"
-                  />
-                  <label htmlFor="Modulo">Modulo</label>
-                </span>
-           
-              </div>
-
-                 <div className="d-flex align-items-center gap-4 mt-4 justify-content-center" style={{ paddingTop: "1.5rem" }}>
-                     {/* Hora */}
-                <span className="p-float-label input-presentacion">
-                  <InputText
-                    className="select"
-                  />
-                  <label htmlFor="Hora">Hora</label>
-                </span>
-                     {/* Fecha */}
-                <span className="p-float-label input-presentacion">
-                  <InputText
-                    className="select"
-                  />
-                  <label htmlFor="Fecha">Fecha</label>
-                </span>
-                 </div>
-   
-      
-              <div className="d-flex justify-content-center gap-3 mt-5 mb-4">
-                <Button icon="pi pi-save" className="p-button-sm small" label="Guardar" severity="success" style={{height: "50px"}} />
-                <Button icon="pi pi-times" className="p-button-sm small" label="Limpiar" severity="danger" style={{height: "50px"}} />
+              <div className="catalogo_operadores w-50 ps-xl-5">
+                <div className="card_elegant_table">
+                  <DataTable
+                    value={ecoDisponibles}
+                    tableStyle={{ minWidth: "100%" }}
+                    rows={5}
+                    paginator
+                    emptyMessage="No hay operadores disponibles"
+                    header={
+                      <div className="d-flex align-items-center table-header-title">
+                        <i className="pi pi-users text-success me-2 fs-5"></i>
+                        <span className="fw-bold text-dark fs-6">
+                          Operadores Disponibles
+                        </span>
+                      </div>
+                    }
+                  >
+                    <Column
+                      field="primer_t"
+                      header="Credencial"
+                      className="text-center"
+                      headerClassName="text-center"
+                    ></Column>
+                    <Column
+                      field="segundo_t"
+                      header="Credencial"
+                      className="text-center"
+                      headerClassName="text-center"
+                    ></Column>
+                    <Column
+                      field="tercer_t"
+                      header="Credencial"
+                      className="text-center"
+                      headerClassName="text-center"
+                    ></Column>
+                    <Column
+                      field="nombre_ruta"
+                      header="RUTA"
+                      className="text-center fw-semibold text-secondary"
+                      headerClassName="text-center"
+                    ></Column>
+                  </DataTable>
+                </div>
               </div>
             </div>
           </div>
+
+          <hr className="linea_punteada" />
         </TabPanel>
       </TabView>
-
-      <div
-        style={{ padding: "20px" }}
-        className="d-flex justify-content-center"
-      >
-        <QRCodeSVG
-          value="https://www.rtp.cdmx.gob.mx/"
-          size={256}
-          bgColor="#ffffff"
-          fgColor="#000000"
-          level="H" // Nivel de corrección de errores (L, M, Q, H)
-          includeMargin={true}
-        />
-      </div>
     </>
   );
 };
